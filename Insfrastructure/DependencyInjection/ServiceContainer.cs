3a56a395de4ada2bde009d04ca58d2ab7a4d9aa5
Minister124 <kushaltmg321@gmail.com>
2024-08-27 14:49:01 +0545

    SSMS connection

--- Insfrastructure/DependencyInjection/ServiceContainer.cs
+++ Insfrastructure/DependencyInjection/ServiceContainer.cs
@@ -1,9 +1,12 @@
+using System.Text;
 using Domain.Entity.Authentication;
 using Insfrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Insfrastructure.DependencyInjection;
 
@@ -18,12 +21,51 @@ public static class ServiceContainer
         services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(config.GetConnectionString("Kushal"))
         );
+
         // Register ASP.NET Core Identity services
         services
-            .AddIdentityCore<AppDbContext>()
-            .AddRoles<IdentityRole>()
-            .AddEntityFrameworkStores<AppDbContext>()
-            .AddSignInManager();
+            .AddIdentityCore<ApplicationUser>() //specify the user type
+            .AddRoles<IdentityRole>() //Add support for Roles
+            .AddEntityFrameworkStores<AppDbContext>() // Use AppDbContext for storing Identity data
+            .AddSignInManager(); // Add SignInManager for handling sign-ins
+
+        //Register JWT Authentication
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true, // Validate the issuer of the token. Ensures the token was issued by a trusted issuer.
+                    ValidateAudience = true, // Validate the audience of the token. Ensures the token is intended for your application.
+                    ValidateIssuerSigningKey = true, // Validate the signing key. Ensures the token was signed with a valid key.
+                    ValidateLifetime = true, // Validate the token's expiration.  Ensures the token hasn't expired.
+                    ValidIssuer = config["Jwt:Issuer"], // Set the valid issuer from configuration. The expected issuer, retrieved from configuration (appsettings.json).
+                    ValidAudience = config["Jwt:Audience"], // Set the valid audience from configuration
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(config["Jwt:Key"]!)
+                    ), // Set the signing key. The key used to sign the token, converted from a string to a symmetric security key.
+                };
+            });
+        services.AddAuthentication();
+        services.AddAuthorization();
+        services.AddCors(o =>
+        {
+            o.AddPolicy(
+                "Client.UI",
+                builder =>
+                    builder
+                        .WithOrigins("https://localhost:7284")
+                        .AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+            );
+        });
 
         // Return the updated IServiceCollection
         return services;
--- Presentation/API/Program.cs
+++ Presentation/API/Program.cs
@@ -1,10 +1,11 @@
-using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Insfrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureService(builder.Configuration);
 
 var app = builder.Build();
 
@@ -15,30 +16,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
