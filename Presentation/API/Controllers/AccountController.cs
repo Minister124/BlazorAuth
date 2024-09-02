a7c5c61a533038700e5495858d57eb995181a76e
Minister124 <kushaltmg321@gmail.com>
2024-09-02 11:11:25 +0545

    Create account in controllers

--- Presentation/API/Controllers/AccountController.cs
+++ Presentation/API/Controllers/AccountController.cs
@@ -1,12 +1,20 @@
+using Application.DTOs.Request.Account;
+using Application.DTOs.Response;
+using Application.IRepository;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountController : ControllerBase
+    public class AccountController(IAccount account) : ControllerBase
     {
-
+        [HttpPost("identity/create")]
+        public async Task<ActionResult<GeneralResponse>> CreateAccount(CreateAccountDTO model){
+            if(!ModelState.IsValid) return BadRequest("Model Cannot be Null");
+            return Ok(await account.CreateAccountAsync(model));
+        }
     }
 }
