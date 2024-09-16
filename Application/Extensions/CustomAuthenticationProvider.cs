01d8fbdfc9e4e8b507bf72a666917928f7210765
Minister124 <kushaltmg321@gmail.com>
2024-09-16 13:52:43 +0545

    Custom Authentication

--- Application/Extensions/CustomAuthenticationProvider.cs
+++ Application/Extensions/CustomAuthenticationProvider.cs
@@ -13,4 +13,24 @@ public class CustomAuthenticationProvider : AuthenticationStateProvider
         _localStorageService = localStorageService;
     }
 
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync(){
+        var tokenModel = await _localStorageService.GetModelFromTokenAsync();
+        if(string.IsNullOrEmpty(tokenModel.Token)) return await Task.FromResult(new AuthenticationState(anonymous));
+
+        var getUserClaims = DecryptToken(tokenModel.Token!);
+        if(getUserClaims == null) return await Task.FromResult(new AuthenticationState(anonymous));
+
+        var ClaimsPrincipal = SetClaimPrincipal(getUserClaims);
+        return await Task.FromResult(new AuthenticationState(ClaimsPrincipal));
+    }
+
+    private ClaimsPrincipal SetClaimPrincipal(object getUserClaims)
+    {
+        throw new NotImplementedException();
+    }
+
+    private object DecryptToken(string v)
+    {
+        throw new NotImplementedException();
+    }
 }
