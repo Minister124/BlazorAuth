7ccf2611bedd8da2b2b6ed30d4890304fb82a23c
Minister124 <kushaltmg321@gmail.com>
2024-09-16 11:35:53 +0545

    Refactored HttpClient

--- Application/Extensions/HttpClientService.cs
+++ Application/Extensions/HttpClientService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http.Headers;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Extensions;
 
@@ -8,36 +9,54 @@ public class HttpClientService
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly LocalStorageService _localStorageService;
 
+    private readonly ILogger<HttpClient> _logger;
+
     public HttpClientService(
         IHttpClientFactory httpClientFactory,
-        LocalStorageService localStorageService
+        LocalStorageService localStorageService,
+        ILogger<HttpClient> logger
     )
     {
         _httpClientFactory = httpClientFactory;
         _localStorageService = localStorageService;
+        _logger = logger;
     }
 
     private HttpClient CreateClient() => _httpClientFactory!.CreateClient(Constants.HttpClientName); //creates a new HttpClient using name Client.UI as Client.UI is the client of my app
 
     public HttpClient GetPublicClient() => _httpClientFactory.CreateClient(); //public access with no authorization for like guest user with no login
 
-    public async Task<HttpClient> GetPrivateClient() //For authorization and for authenticated request using Bearer authentication scheme
+    public async Task<HttpClient> GetPrivateClientAsync() //For authorization and for authenticated request using Bearer authentication scheme
     {
         try
         {
             var client = CreateClient();
-            var localStorage = await _localStorageService.GetModelFromTokenAsync();
-            if (string.IsNullOrEmpty(localStorage.Token))
-                return client;
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-                Constants.HttpClientHeaderScheme,
-                localStorage.Token
-            );
+            //Fecth token from local storage and set authorization header
+            var token = await GetTokenAsync();
+            if(!string.IsNullOrEmpty(token)){
+                SetAuthorizationHeader(client, token);
+            }
             return client;
         }
-        catch
+        catch (Exception ex)
         {
-            return new HttpClient();
+            _logger.LogError(ex, "Failed to create private client");
+            return null;
         }
+    }
+
+    //Gets the token from local storage
+    private async Task<string?> GetTokenAsync()
+    {
+        var localStorage = await _localStorageService.GetModelFromTokenAsync();
+        return localStorage.Token;
+    }
+
+    private void SetAuthorizationHeader(HttpClient httpClient, string token)
+    {
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            Constants.HttpClientHeaderScheme,
+            token
+        );
     }
 }
