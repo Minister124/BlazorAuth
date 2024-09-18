73bf991f4b2b0ac191cc1d20bdb43f174c65ec5c
Minister124 <kushaltmg321@gmail.com>
2024-09-18 11:53:10 +0545

    Backend done

--- Presentation/Client.UI/Program.cs
+++ Presentation/Client.UI/Program.cs
@@ -2,13 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client.UI;
 using MudBlazor.Services;
+using MudBlazor;
+using Application.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddApplicationService();
 builder.Services.AddMudServices();
-
+builder.Services.AddScoped<MudAlert>();
 
 await builder.Build().RunAsync();
